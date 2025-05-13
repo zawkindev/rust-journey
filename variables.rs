90b5fdf6b55414a4f2f62ecae8a3e7e738712f8c
zawkindev <zawkindev@gmail.com>
2025-05-13 22:21:45 +0500

    variables

---
+++ variables.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let x = 5;
+    println!("x: {}", x);
+
+    let mut y = 5;
+    println!("y: {}", y);
+
+    y = 6;
+    println!("y == {}", y);
+
+    let a: i32 = 4;
+    println!("a: {}", a);
+
+    let a = "wahahaha";
+    println!("a: {}", a);
+}
