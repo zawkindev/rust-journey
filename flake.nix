f8758a6a6bfedf647cb2294e2bb7a73b1b7d1424
zawkindev <zawkindev@gmail.com>
2025-05-13 22:15:07 +0500

    hello world

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "A simple Rust dev shell using Nix Flakes";
+
+  inputs = {
+    # Use the unstable channel for the latest packages
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    # flake-utils to support multiple systems easily
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.cargo
+            pkgs.rustfmt
+            pkgs.clippy
+            pkgs.rustc
+          ];
+          shellHook = ''
+            echo "Welcome to Rust on ${system}!"
+            zsh
+          '';
+        };
+      }
+    );
+
+}
+
