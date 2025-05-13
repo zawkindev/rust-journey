87265174321d8c6381240d066a852bd5bb314005
zawkindev <zawkindev@gmail.com>
2025-05-13 23:47:31 +0500

    more enums

--- enums.rs
+++ enums.rs
@@ -20,4 +20,40 @@ fn main() {
     boot(linux);
     boot(macos);
     boot(windows);
+
+    enum Woman {
+        Wife(String),
+        Waifu { name: String, age: u8 },
+        Girl,
+    }
+
+    let tya = Woman::Wife(String::from("Tya"));
+
+    let asuna = Woman::Waifu {
+        name: String::from("Asuna"),
+        age: 18,
+    };
+
+    let random_girl = Woman::Girl;
+
+    impl Woman {
+        fn name(&self) -> String {
+            match self {
+                Woman::Wife(name) => name.clone(),
+                Woman::Waifu { name, .. } => name.clone(),
+                Woman::Girl => "Some Random Girl".to_string(),
+            }
+        }
+
+        fn age(&self) -> u8 {
+            match self {
+                Woman::Waifu { age, .. } => *age,
+                _ => 0,
+            }
+        }
+    }
+
+    println!("{} is {} years old", asuna.name(), asuna.age());
+    println!("My wife's name is {}! ", tya.name());
+    println!("Some {} passed by me yesterday", random_girl.name());
 }
